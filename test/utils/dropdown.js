87f4b45bafac0857fcd2b43249141c146d4f887f
austin <austin.black.55@gmail.com>
2023-09-12 23:57:35 -0500

    complete task 5 for webdriverio assignment
    created dropdown.js, navigates to dropdown page and selects a random
    value

---
+++ test/utils/dropdown.js
@@ -0,0 +1,22 @@
+import { remote } from "webdriverio";
+import { getRandomNum } from "./util.js";
+
+const browser = await remote({
+    capabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: process.env.CI ? ['disable-gpu'] : []
+        }
+    }
+})
+
+async function selectDropdown() {
+    const randomNum = getRandomNum(2);
+    await browser.url("http://the-internet.herokuapp.com/dropdown");
+    const dropdown = await browser.$('#dropdown');
+    await dropdown.selectByVisibleText(`Option ${randomNum}`)
+    await browser.pause(2000); // normally hate pauses but keeping this one in so you can verify an option was selected
+    await browser.deleteSession();
+}
+
+selectDropdown();
